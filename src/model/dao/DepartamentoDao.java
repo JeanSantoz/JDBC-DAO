65d465afe7dd98728be232ffa4d203efc153571b
Jean Ferreira <Jeansantoz13@gmail.com>
2022-07-30 07:57:21 -0300

    interfaces dao criadas

---
+++ src/model/dao/DepartamentoDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+import model.entidades.Departamento;
+
+public interface DepartamentoDao {
+    
+    void insert(Departamento departamento);
+    void update(Departamento departamento);
+    void deleteById(Integer id);
+    Departamento findById(Integer id);
+    List<Departamento> findAll();
+
+}
